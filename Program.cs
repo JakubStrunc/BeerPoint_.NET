0308c87eb3ce9f5209ec4e5cbe5f76fe5a2caa73
jakubs <kubaelis@gmail.com>
2025-04-23 12:30:09 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+using PNET_semestralka_blazor_app.Components;
+using PNET_semestralka_blazor_app.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace PNET_semestralka_blazor_app
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            var controllerTypes = Assembly.GetExecutingAssembly()
+                .GetTypes()
+                .Where(t => t.IsClass && !t.IsAbstract && t.Name.EndsWith("Controller"));
+
+            foreach (var type in controllerTypes)
+            {
+                builder.Services.AddScoped(type);
+            }
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
